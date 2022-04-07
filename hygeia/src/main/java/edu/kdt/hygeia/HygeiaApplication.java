289c25fcbb6ec0ca8cebae699ba6a061983e0218
saeha <togk1996@gmail.com>
2022-04-07 16:21:20 +0900

    first

--- hygeia/src/main/java/edu/kdt/hygeia/HygeiaApplication.java
+++ hygeia/src/main/java/edu/kdt/hygeia/HygeiaApplication.java
@@ -8,20 +8,6 @@ public class HygeiaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HygeiaApplication.class, args);
-		//hi github
-		// conflict test 1
-		// git test
-		// conflict test 3
-		// conflict test 2
-		// conflict test 100
-		// conflict test 101
-		// conflict test 3
-		// conflict test 2
-		// 또잉또잉
-		// conflict test 4
-		// conflict 5
-		// conflict SH 1
-		// conflict SH 2
 
 	}
 
