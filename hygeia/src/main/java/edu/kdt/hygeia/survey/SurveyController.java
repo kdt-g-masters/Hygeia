bd60746d39d90e4586023115b5fbcf09c65695e6
DESKTOP-434QLLV\whdan <whdan33@naver.com>
2022-04-14 17:20:22 +0900

    커밋커밋커밋

--- hygeia/src/main/java/edu/kdt/hygeia/survey/SurveyController.java
+++ hygeia/src/main/java/edu/kdt/hygeia/survey/SurveyController.java
@@ -1,6 +1,26 @@
 package edu.kdt.hygeia.survey;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SurveyController {
+	
+	@RequestMapping("/survey1")
+		public String surveyMain() {
+			return "surveyMain"; 
+		}
+
+	@RequestMapping("/survey2")
+		public String surveyDoing() {
+			return "surveyDoing"; 
+		}
+
+	@RequestMapping("/survey3")
+		public String surveyResult() {
+			return "surveyResult"; 
+		}
+
 
 	// Test 1
 	// Test 2
