fadd29817c871febb2ffc95a5ea3a0a760c6412c
DESKTOP-434QLLV\whdan <whdan33@naver.com>
2022-04-18 15:42:16 +0900

    DAO 파일 커밋커밋

--- hygeia/src/main/java/edu/kdt/hygeia/survey/SurveyDAO.java
+++ hygeia/src/main/java/edu/kdt/hygeia/survey/SurveyDAO.java
@@ -1,5 +1,12 @@
 package edu.kdt.hygeia.survey;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository("SurveyDAO")
 public interface SurveyDAO {
+	
+	public int saveResult(SurveyDTO dto); 
 
 }
